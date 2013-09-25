c0ba5dc14e4bfd96eca413db797713f9407f3fc2
Stanislas Guerra <stanislas.guerra@gmail.com>
2013-09-25 17:53:02 +0200

    Add admin.py in test_app.

---
+++ tests/django_test_project/test_app/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from test_app import models
+
+
+class BandAdmin(admin.ModelAdmin):
+    pass
+
+
+class BandMemberAdmin(admin.ModelAdmin):
+    raw_id_fields = ("band",)
+
+
+admin.site.register(models.Band, BandAdmin)
+admin.site.register(models.BandMember, BandMemberAdmin)
