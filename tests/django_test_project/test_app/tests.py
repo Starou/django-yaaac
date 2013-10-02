d3712d76803561f04527274e3e1fbc53d7bf3059
Stanislas Guerra <stanislas.guerra@gmail.com>
2013-10-02 14:31:20 +0200

    Add a Selenium test (no assertions atm.)

--- tests/django_test_project/test_app/tests.py
+++ tests/django_test_project/test_app/tests.py
@@ -1,6 +1,6 @@
 import json
 from django.contrib.auth.models import User
-from django.test import TestCase
+from django.test import TestCase, LiveServerTestCase
 from django.test.client import Client
 
 
@@ -13,3 +13,23 @@ class AutocompleteTest(TestCase):
        response = self.client.get("/yaaac/7/search/?query=gene") 
        self.assertEqual(json.loads(response.content),
                         {u'data': [1], u'query': u'gene', u'suggestions': [u'Genesis']})
+
+
+class LiveServerTest(LiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.selenium = WebDriver()
+        super(LiveServerTest, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        super(LiveServerTest, cls).tearDownClass()
+        cls.selenium.quit()
+
+    def setUp(self):
+        super(LiveServerTest, self).setUp()
+
+    def test_foreign_key_autocomplete(self):
+        self.selenium.get('%s/' % self.live_server_url)
+        band_input = self.selenium.find_element_by_name("band")
+        band_input.send_keys("the ")
