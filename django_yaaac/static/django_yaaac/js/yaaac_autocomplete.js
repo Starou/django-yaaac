ae575dc72099cbb7080fd4a0447021aef173b7ad
Stanislas Guerra <stanislas.guerra@gmail.com>
2013-10-02 18:21:53 +0200

    update the value label when choosing a related object and hide the search input. The search input is shown when the user click on the clear button next to the label value.

--- django_yaaac/static/django_yaaac/js/yaaac_autocomplete.js
+++ django_yaaac/static/django_yaaac/js/yaaac_autocomplete.js
@@ -1,10 +1,40 @@
+var yaaac_set_label = function() {
+    /* Display the unicode() value of the instance and hide the autocomplete input. */
+    var $id_input = $(this);
+    if ($id_input.val() !== "") {
+        $.get($id_input.attr("search_url"), {value_attr: $id_input.attr("value_attr"), pk: $id_input.val()},
+              function(data){
+                var search_input = $id_input.next();
+                var value_container = search_input.next();
+                var value_elem = value_container.find(".yaaac_value");
+                value_elem.html(data.value);
+
+                search_input.hide();
+                value_container.show();
+        });
+    }
+};
+
+
+var yaaac_clear_value = function() {
+    /* Clear the FK field and switch to autocomplete search mode. */
+    $(this).parents(".yaaac_container").find(".yaaac_pk").val("");
+    $(this).parent().hide();
+    $(this).parents(".yaaac_container").find(".yaaac_search_input").val("").show();
+};
+
+
 $(document).ready(function() {
+    $(".yaaac_pk").on("change", yaaac_set_label);
+    $(".yaaac_clear_value").on("click", yaaac_clear_value);
+
     $(".yaaac_search_input").each(function() {
+        // $id_input is the input.yaaac_pk elem.
         $id_input = $(this).prev();
         var options = {
             serviceUrl: $id_input.attr("search_url"),
             onSelect: function(suggestion) {
-                $id_input.val(suggestion.data);
+                $id_input.val(suggestion.data).change();
             },
             params: {
                 value_attr: $id_input.attr("value_attr")
