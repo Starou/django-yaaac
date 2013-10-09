2b9162c7fdd9b1c20665b9827bb525a8900ac7e3
Stanislas Guerra <stanislas.guerra@gmail.com>
2013-10-09 16:31:49 +0200

    admin-enable - create custom admins in test project (one with limit_choices_to and one without.)

--- tests/django_test_project/django_test_project/urls.py
+++ tests/django_test_project/django_test_project/urls.py
@@ -1,10 +1,11 @@
 import django_yaaac as yaaac
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
-admin.autodiscover()
+
+from my_admin import limit_choices_site, autocomplete_site
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
+    (r'^limit-choices-admin/', include(limit_choices_site.urls)),
+    (r'^admin/', include(autocomplete_site.urls)),
     url(r'^yaaac/', include(yaaac.autocomplete.urls)),
     url(r'^band-member-form/$', "test_app.views.band_member_form"),
     url(r'^band-member-form/(?P<member_id>\d+)/$', "test_app.views.band_member_form"),
