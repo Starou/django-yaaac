50279436447c7b7eab18fc7dffc9936640bb9031
Stanislas Guerra <stanislas.guerra@gmail.com>
2013-12-23 14:38:13 +0100

    Bump to 0.93.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-yaaac',
-    version='0.92.5',
+    version='0.93.0',
     license='BSD Licence',
     author='Stanislas Guerra',
     author_email='stanislas.guerra@gmail.com',
