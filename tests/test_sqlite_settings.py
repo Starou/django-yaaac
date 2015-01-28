8d68826a2f2ac37ccee357f3d9620e071f442dcf
Stanislas Guerra <stanislas.guerra@gmail.com>
2015-01-28 16:21:53 +0100

    Travis gives some weird error with in-memory sqlite. Try a file database.

--- tests/test_sqlite_settings.py
+++ tests/test_sqlite_settings.py
@@ -1,6 +1,13 @@
+import os
+CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(CURRENT_DIR, 'database.db'),
+        'TEST': {
+            'NAME': os.path.join(CURRENT_DIR, 'test_database.db'),
+        }
     },
 }
 SECRET_KEY = 'i0_+-t@@wul&q)30+4y)8-19s)31@%cv8$q(c@8q1g#h$6wn-='
