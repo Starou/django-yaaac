b16a7b3c7eeb63b24523c240a4e1ac152c847079
Stanislas Guerra <stanislas.guerra@gmail.com>
2015-01-28 15:25:21 +0100

    Removed settings.DATABASES['other'] in test_sqlite_settings.py.

--- tests/test_sqlite_settings.py
+++ tests/test_sqlite_settings.py
@@ -2,9 +2,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3'
     },
-    'other': {
-        'ENGINE': 'django.db.backends.sqlite3',
-    }
 }
 SECRET_KEY = 'i0_+-t@@wul&q)30+4y)8-19s)31@%cv8$q(c@8q1g#h$6wn-='
 USE_TZ = True
