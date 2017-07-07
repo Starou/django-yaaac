c3e44315c9b07508f76dce487e337272c46387de
Stanislas Guerra <sguerra@figarocms.fr>
2017-07-07 11:41:30 +0200

    Fix dismiss of required attribute on the search input when using the popup.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-yaaac',
-    version='1.10.1',
+    version='1.10.2',
     license='BSD Licence',
     author='Stanislas Guerra',
     author_email='stanislas.guerra@gmail.com',
